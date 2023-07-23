df91af2ae77b32cc5c23a678c206e0e19cfa2070
ElBrenkel <41021512+ElBrenkel@users.noreply.github.com>
2023-07-23 15:12:59 +0300

    Update solution

--- IsPalindrome/is_palindrome.py
+++ IsPalindrome/is_palindrome.py
@@ -3,16 +3,7 @@ import re
 
 def is_palindrome(s):
     clean_sentence = re.sub(r'[\W_]', '', s).lower()
-    i = 0
-    j = len(clean_sentence) - 1
-
-    while i < j:
-        if clean_sentence[i] != clean_sentence[j]:
-            return False
-        i += 1
-        j -= 1
-
-    return True
+    return clean_sentence == clean_sentence[::-1]
 
 
 print(is_palindrome("0P"))
